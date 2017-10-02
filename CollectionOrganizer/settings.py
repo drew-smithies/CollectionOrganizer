6a9ec149ef83ea31acdb4bc9efae3397073b7c6d
Drew Smithies <dschevyvette@gmail.com>
2017-10-02 16:51:42 -0500

    Tools sample

--- CollectionOrganizer/settings.py
+++ CollectionOrganizer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_mongoengine',
     'api'
 ]
 
@@ -75,12 +77,14 @@ WSGI_APPLICATION = 'CollectionOrganizer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+mongoengine.connect('tools_sample')
 
 
 # Password validation
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import CreateView
-from .views import DetailsView 
+from api.views import *
 
 urlpatterns = {
-    url(r'^bucketlists/$', CreateView.as_view(), name="create"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$',
-        DetailsView.as_view(), name="details")
+    url(r'^tool/$', ToolViewSet, name="create"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
