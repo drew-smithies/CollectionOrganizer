0c79f711f4da87b0aeb6e2d9f251029d82fae380
Drew Smithies <dschevyvette@gmail.com>
2017-10-04 17:23:22 -0500

    Use DRF mongo router

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework import routers
+from rest_framework_mongoengine import routers
 from api import views 
 
 router = routers.DefaultRouter()
