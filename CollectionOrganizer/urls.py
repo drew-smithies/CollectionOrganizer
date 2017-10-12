e709aab7fa9991af66337b15e58221154e59c26d
Drew Smithies <dschevyvette@gmail.com>
2017-10-12 17:48:24 -0500

    Namespacing api urls

--- CollectionOrganizer/urls.py
+++ CollectionOrganizer/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework.authtoken import views as auth_views
 
 router = routers.DefaultRouter()
 
@@ -25,5 +24,5 @@ router = routers.DefaultRouter()
 # Browsable API URL: url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api/' + settings.API_VERSION + '/auth/', auth_views.obtain_auth_token)
+    url(r'^api/' + settings.API_VERSION, include('api.urls', namespace='api'))
 ]
