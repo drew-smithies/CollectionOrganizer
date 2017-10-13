bcc0f1d417145127e698235841876bffca163f29
Drew Smithies <dschevyvette@gmail.com>
2017-10-13 17:51:57 -0500

    Created custom user

--- CollectionOrganizer/settings.py
+++ CollectionOrganizer/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 API_VERSION = 'v1'
+AUTH_USER_MODEL = 'api.User'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
