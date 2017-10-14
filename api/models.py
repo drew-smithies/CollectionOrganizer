678ed7c4f051f1bbf2b2d713641700f5e00fefbb
Drew Smithies <dschevyvette@gmail.com>
2017-10-13 21:49:54 -0500

    Added more admin functionality

--- api/models.py
+++ api/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-  collection_count = models.IntegerField(default=0) 
+  collection_count = models.IntegerField(default=0)
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.authtoken import views as auth_views
 
 
 app_name = "api"
 urlpatterns = [
-    url(r'/auth/', auth_views.obtain_auth_token, name="auth")
+    url(r'auth/', auth_views.obtain_auth_token, name="auth")
 ]
