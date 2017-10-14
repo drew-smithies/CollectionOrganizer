aed997e42b4c8ef12ee405ca92728187d0ec5e3f
Drew Smithies <dschevyvette@gmail.com>
2017-10-14 18:39:49 -0500

    Added collection to admin + migration

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Collection
 
 
 # Register your models here.
+@admin.register(Collection)
+class CollectinAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+  list_display_links = ('name',)
+
 admin.site.register(User, UserAdmin)
--- api/models.py
+++ api/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 # Create your models here.
 
 class User(AbstractUser):
   collection_count = models.IntegerField(default=0)
+
+class Collection(models.Model):
+  user = models.ForeignKey(
+      User,
+      models.SET_NULL,
+      blank=True,
+      null=True,
+  )
+  name = models.CharField(max_length=64)
+  description = models.TextField(max_length=256)
