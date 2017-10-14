60002771b0959603c2cc366080385d5e89bcbd00
Drew Smithies <dschevyvette@gmail.com>
2017-10-13 21:37:49 -0500

    Added admin back

--- CollectionOrganizer/urls.py
+++ CollectionOrganizer/urls.py
@@ -3,6 +3,8 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 """
+
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import url, include
 from rest_framework import routers
@@ -12,8 +14,9 @@ router = routers.DefaultRouter()
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 # Browsable API URL: 
+#    url(r'^browser/', include('rest_framework.urls', namespace='rest_framework')),
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^browser/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/' + settings.API_VERSION, include('api.urls', namespace='api'))
 ]
