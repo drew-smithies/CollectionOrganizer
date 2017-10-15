42d07ddf107dac9af26def5e54734347aa3eb502
Drew Smithies <dschevyvette@gmail.com>
2017-10-14 21:25:29 -0500

    Added models  and admin models

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Collection
+from .models import User, Collection, Item, Attribute, Category
 
 
 # Register your models here.
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+  list_display = ('username', 'collection_count')
+  list_display_links = ('username',)
+
 @admin.register(Collection)
 class CollectinAdmin(admin.ModelAdmin):
-  list_display = ('name',)
+  list_display = ('name', 'user', 'description', 'item_count')
+  list_display_links = ('name', 'user',)
+  readonly_fields = ('item_count',)
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ('name', 'collection', 'description')
   list_display_links = ('name',)
 
-admin.site.register(User, UserAdmin)
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+  list_display = ('id', 'category', 'value', 'item')
+  list_display_links = ('id',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'collection')
+  list_display_links = ('name',)
--- api/models.py
+++ api/models.py
@@ -2,15 +2,63 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+''' User '''
 class User(AbstractUser):
   collection_count = models.IntegerField(default=0)
 
+''' Collection '''
 class Collection(models.Model):
   user = models.ForeignKey(
       User,
-      models.SET_NULL,
-      blank=True,
-      null=True,
+      models.CASCADE,
   )
   name = models.CharField(max_length=64)
   description = models.TextField(max_length=256)
+  item_count = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.name
+
+''' Item '''
+class Item(models.Model):
+  collection = models.ForeignKey(
+      Collection,
+      models.CASCADE,
+  )
+  name = models.CharField(max_length=64)
+  description = models.TextField(max_length=256)
+  # Todo: add picture, notes
+
+  def __str__(self):
+    return self.name
+
+''' Category '''
+class Category(models.Model):
+  name = models.CharField(max_length=64)
+  collection = models.ForeignKey(
+      Collection,
+      models.SET_NULL,
+      null=True,
+      blank=True,
+  )
+
+  def __str__(self):
+    return self.name
+
+''' Attribute '''
+class Attribute(models.Model):
+  item = models.ForeignKey(
+      Item,
+      models.CASCADE,
+  )
+  category = models.ForeignKey(
+      Category,
+      models.SET_NULL,
+      null=True,
+      blank=True,
+  )
+  value = models.TextField(max_length=256)
+
+  def __str__(self):
+    return self.value
+  
