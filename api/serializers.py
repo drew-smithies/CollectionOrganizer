3aa27e25c8fde7d0fe90120cc6947a2440918061
Drew Smithies <dschevyvette@gmail.com>
2017-10-16 16:18:36 -0500

    Add password confirm to create user

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+  password_confirm = serializers.CharField()
   class Meta:
     model = User
     fields = '__all__'
+
+  def validate(self, data):
+    '''
+    Check that passwords match
+    '''
+    if data['password'] != data['password_confirm']:
+      raise serializers.ValidationError('Oops, your passwords did not match, try again.')
+    return data
