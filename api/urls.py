aca48becc5f31da5fd4da3cfcdd9498c6429ae01
Drew Smithies <dschevyvette@gmail.com>
2017-10-16 16:18:14 -0500

    Added generic user views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from rest_framework.authtoken import views as auth_views
+from api import views
 
 
 app_name = "api"
 urlpatterns = [
-    url(r'auth/', auth_views.obtain_auth_token, name="auth")
+    url(r'auth/', auth_views.obtain_auth_token, name="auth"),
+    url(r'user/$', views.UserList.as_view()),
+    url(r'user/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
