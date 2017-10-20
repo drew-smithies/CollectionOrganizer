8d474080e9322034ed88b064ff63bdf30ce0e5e1
Drew Smithies <dschevyvette@gmail.com>
2017-10-20 01:48:20 -0500

    Merged user serializers and view; regenerate token on password update

--- api/serializers.py
+++ api/serializers.py
@@ -2,23 +2,24 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from .models import User
 
-class UserCreateSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
   password_confirm = serializers.CharField(write_only=True)
 
   class Meta:
     model = User
     fields = '__all__'
 
-  def validate(self, data):
+  def validate(self, attrs):
     '''
-      Check that passwords match
+      Validate password and password_confirm
     '''
-    if data['password'] and not data['password_confirm']:
-      raise serializers.ValidationError('Please confirm your password.')
-    if data['password'] != data['password_confirm']:
-      raise serializers.ValidationError('Oops, your passwords did not match, try again.')
-    data.pop('password_confirm')
-    return data
+    if 'password' in attrs:
+      if not 'password_confirm' in attrs:
+        raise serializers.ValidationError('Please confirm your password.')
+      elif attrs['password'] != attrs['password_confirm']:
+        raise serializers.ValidationError('Oops, your passwords did not match, try again.')
+      attrs.pop('password_confirm')
+    return attrs
 
   def create(self, validated_data):
     '''
@@ -27,41 +28,26 @@ class UserCreateSerializer(serializers.ModelSerializer):
     password = validated_data.pop('password', None)
     instance = self.Meta.model(username=validated_data['username'], email=validated_data['email'])
     if password is not None:
+      # Set password and create token
       instance.set_password(password)
+      Token.objects.create(user=instance)
     instance.save()
-
-    # Create token
-    Token.objects.create(user=instance)
 
     return instance
 
-class UserSerializer(serializers.ModelSerializer):
-  password_confirm = serializers.CharField(write_only=True)
-
-  class Meta:
-    model = User
-    fields = '__all__'
-
-  def validate(self, data):
-    '''
-      If password being pudated, check that passwords match
-    '''
-    if 'password' in data:
-      if not 'password_confirm' in data:
-        raise serializers.ValidationError('Please confirm your password.')
-      elif data['password'] != data['password_confirm']:
-        raise serializers.ValidationError('Oops, your passwords did not match, try again.')
-      data.pop('password_confirm')
-    return data
-
   def update(self, instance, validated_data):
     '''
-      If password is updated, hash it
+      Update User
     '''
     for attr, value in validated_data.items():
       if attr == 'password':
+        # Update password, delete token and create a new one
         instance.set_password(value)
+        Token.objects.get(user=instance.pk).delete()
+        Token.objects.create(user=instance)
       else:
         setattr(instance, attr, value)
     instance.save()
+
+
     return instance
--- api/views.py
+++ api/views.py
@@ -1,28 +1,10 @@
 from rest_framework import generics
-from rest_framework import mixins
 from .models import User
-from .serializers import UserSerializer, UserCreateSerializer
+from .serializers import UserSerializer
 
-class UserListCreate(mixins.CreateModelMixin,
-                     mixins.ListModelMixin,
-                     generics.GenericAPIView):
+class UserListCreate(generics.ListCreateAPIView):
   queryset = User.objects.all()
   serializer_class = UserSerializer
-
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-
-  def get_serializer_class(self):
-    """
-      Use differnt serializers for different methods
-    """
-    if self.request.method == 'POST':
-      return UserCreateSerializer
-    return UserSerializer
-
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = User.objects.all()
